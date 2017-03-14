95fc9e0c4f441039129b093082832c6b3437fe99
DESKTOP-0QH6U6U\Fama0 <fama0501@hotmail.com>
2017-03-14 22:28:29 +0100

    First Commit

---
+++ Source/BeTheGoalkeeper/BeTheGoalkeeperGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BeTheGoalkeeperGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BETHEGOALKEEPER_API ABeTheGoalkeeperGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
